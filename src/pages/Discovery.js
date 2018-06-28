fbb20904cf918604cf87d9a6681e838a6da7fed0
Daniel Beck <nowseemee@gmail.com>
2018-06-28 17:29:50 +0200

    feat: fetchSearchToState

--- src/pages/Discovery.js
+++ src/pages/Discovery.js
@@ -13,6 +13,36 @@ class Discovery extends React.Component {
     })
   };
 
+  state = {
+    items: [],
+    hasError: false,
+    isLoading: false
+  };
+
+  componentDidMount() {
+    this.fetchSearchToState(this.props.match.params.q);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    nextProps.match.params.q !== this.props.match.params.q &&
+      this.fetchSearchToState(nextProps.match.params.q);
+  }
+
+  fetchSearchToState = q => {
+    this.setState(
+      () => ({ isLoading: true }),
+      () =>
+        fetch(`https://api.github.com/search/repositories?per_page=6&q=${q}`)
+          .then(response => response.json())
+          .then(({ items }) =>
+            this.setState(() => ({ items, hasError: false, isLoading: false }))
+          )
+          .catch(() =>
+            this.setState(() => ({ hasError: true, isLoading: false }))
+          )
+    );
+  };
+
   render() {
     return (
       <div>
@@ -21,6 +51,16 @@ class Discovery extends React.Component {
           q={this.props.match.params.q}
           onSubmit={this.props.history.push}
         />
+
+        {this.state.hasError && (
+          <h2>Something went wrong, please try again...</h2>
+        )}
+
+        {this.state.isLoading && <h2>Loading...</h2>}
+
+        {this.state.items.map(item => {
+          return item.full_name;
+        })}
       </div>
     );
   }
