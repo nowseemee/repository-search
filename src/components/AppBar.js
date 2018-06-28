acd2c9dd630528e3f7512da5d16079d145f486d1
Daniel Beck <nowseemee@gmail.com>
2018-06-28 19:49:04 +0200

    feat: add link to home

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -2,13 +2,14 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
 
 export default () => (
   <AppBar position="static" color="default">
     <Toolbar>
-      <Typography variant="title" color="inherit">
-        Code Discovery
-      </Typography>
+      <Link style={{ color: "black", textDecoration: "none" }} to={"/"}>
+        <Typography variant="title">Code Discovery</Typography>
+      </Link>
     </Toolbar>
   </AppBar>
 );
